f3ab7e0c9fc899679a6282acb89b30eabd28eabd
kevinkumar2001 <kevinkum.ar0a2@gmail.com>
2024-06-27 19:29:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Rolex Bot Started!"})
 
 def run_server():
     app.run(host='0.0.0.0', port=80)
