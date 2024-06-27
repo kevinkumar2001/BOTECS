90e4b5dbcde56ae152f2370aface753d1d00cc34
kevinkumar2001 <kevinkum.ar0a2@gmail.com>
2024-06-27 19:28:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Hello, World!"})
 
-if __name__ == '__main__':
+def run_server():
     app.run(host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
+    run_server()
